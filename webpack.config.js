e37350825c400dbb5d263773bce5a69febba0bcc
sernprojects <sernprojects@gmail.com>
2021-02-25 21:52:13 +0000

    added css sass postcss browserslist and fix the bug on hot reloading in prefixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const currentTask = process.env.npm_lifecycle_event
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+let target = "web"
 
 let config = {
   devtool: "source-map",
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
       },
       {
         test: /\.js$/,
@@ -27,10 +28,12 @@ if (currentTask == "start") {
     hot: true,
   }
   config.mode = "development"
+  config.target = "web"
 }
 
 if (currentTask == "build") {
   config.mode = "production"
+  config.target = "browserslist"
 }
 
 if (currentTask == "build-dev") {
